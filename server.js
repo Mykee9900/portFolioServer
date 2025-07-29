17737e4547a9b8e6616334b81e3b278b50bbe9ad
Kikes9900 <mykee5511@gmail.com>
2025-07-28 19:46:21 -0600

    making changes to cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://main.d1dxxawjmxdrus.amplifyapp.com/'],
+  origin: ['http://localhost:3000', process.env.FRONTEND_ORIGIN],
   methods: ['POST'],
 }));app.use(express.json());
 
